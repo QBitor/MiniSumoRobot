1c2c549b75d37069eaa91f8e09675b7a88742f75
QBitor <nicolasdouard@gmail.com>
2017-04-05 03:09:52 +0200

    Cleanup & add start button

--- Robot1/Robot1.ino
+++ Robot1/Robot1.ino
@@ -1,217 +1,229 @@
+//POWER SWITCH definitions
+int powerSwitch_Pin = 12;
+
 //US defintions
 #include <Ultrasonic.h>
-#define TRIGGER_PIN  6
-#define ECHO_PIN     7
+#define TRIGGER_PIN  9
+#define ECHO_PIN     11
 Ultrasonic ultrasonic(TRIGGER_PIN, ECHO_PIN);
 
-//SERVO definitions
-#include <Servo.h> //include servo libs
-Servo Servo; //create servo object to control a servo 
-int Servo_Pin = 9; //connected to digital out 2
+//MOTOR definitions
+int E1 = 5;   //left motor PWM   
+int M1 = 4;   //left motor 
+int E2 = 3;   //right motor PWM                   
+int M2 = 2;   //right motor
 
 //LINE defintions 
-int leftFrontLine_Pin = 0; //connected to analog in 0
-int rightFrontLine_Pin = 1; //connected to analog in 0
-
+int rightFrontLine_Pin = 4; //connected to analog in 0
 int frontLine_Led = 13;
+int rightFrontLine_Value;
 int frontLine_Value;
 
 //GLOBAL definitions
-//arena official diameter micro category: 38.5 cm
+//arena official diameter mini category: 77 cm
 float trigDist = 38.5f;
 boolean trigToken;
 float t1, t2, t3;
 float maxTime;
 boolean noBreak;
 
-void setup()
-{
-  pinMode(frontLine_Led, OUTPUT); //frontLine associated LED
-  
-  Serial.begin(9600);
-
-  //startup delay
-  Serial.println("Welcome to NeoThug 1.2");
-  Serial.println("Starting in 5 seconds...");
-  delay(5000);
-  AttachServos();
-  Serial.println("Motors enabled");
-  Stop();
+void setup(){
+	Serial.begin(9600);
+	//startup delay
+	Serial.println("Welcome to Neothug");
+	pinMode(M1, OUTPUT);   
+	pinMode(M2, OUTPUT); 
+	while(!PowerPush()){
+		Serial.println("Waiting for start switch!");
+	}
+	Serial.println("Starting in 5 seconds...");
+	delay(5000);
 }
 
-void loop2(){
-  US();
-  Serial.print("FrontLine Sensor: ");
-  Serial.println(analogRead(frontLine_Pin));
-  Stop();
+boolean PowerPush(){
+	if (digitalRead(powerSwitch_Pin) == LOW) {
+		Serial.print("Button is LOW");
+		return true; //robot run
+	}
+	else{
+		Serial.print("Button is HIGH");
+		return false;
+	}
 }
 
-void loop()
-{  
-  //FrontLine true when white, false when black in real conditions
-  Serial.print("FrontLine Sensor: ");
-  Serial.println(analogRead(frontLine_Pin));
-  if (FrontLine()){
-    //led ON
-    digitalWrite(frontLine_Led, HIGH);
-  }
-  else{
-    //led OFF
-    digitalWrite(frontLine_Led, LOW);
-  }
-
-  //main code
-
-  //get current values
-  if(!trigToken){
-    t1 = US();
-    delay(5);
-    t2 = US();
-    delay(5);
-    t3 = US();
-    delay(5);
-  }
-  
-  if(!FrontLine()){
-    if ((t1 < trigDist && t2 < trigDist && t3 < trigDist) || trigToken){ 
-      trigToken = false; 
-      maxTime = millis() + 3500.0f;
-      while(!FrontLine() && millis() < maxTime){
-        Forward();
-      }
-
-      
-//      noBreak = true;
-//      while(!FrontLine() && noBreak){
-//        EvasiveManeuver();
-//      }
-    }
-    else{
-      while(!FrontLine() && US() > trigDist){
-        RotateCW();
-      }
-      Stop();
-      if (US() < trigDist){
-        trigToken = true;
-      }
-    }
-  }
-  
-  if(FrontLine()){
-    Stop();
-    AvoidBorder();
-  }
+void SensorsReport(){
+	US();
+	delay(1000);
+	Serial.print("Right frontLine Sensor: ");
+	Serial.println(analogRead(rightFrontLine_Pin));
+	delay(1000);
+	Serial.print("PowerPush state: ");
+	delay(1000);
+	PowerPush();
 }
 
+void TestMotors(){ 
+	int value; 
+	for(value = 0 ; value <= 255; value+=5){ 
+		digitalWrite(M1,LOW);   
+		digitalWrite(M2, LOW);       
+		analogWrite(E1, value);   //PWM Speed control 
+		analogWrite(E2, value);   //PWM Speed control 
+		delay(30); 
+	}  
+}
 
-float US()
-  {
-  //US
-  float cmMsec, inMsec;
-  long microsec = ultrasonic.timing();
+void testloop(){
+	//SensorsReport();
+	//TestMotors(); 
+}
 
-  cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);
-  Serial.print("MS: ");
-  Serial.print(microsec);
-  Serial.print(", Distance (cm): ");
-  Serial.println(cmMsec);
-  return cmMsec;
-  }
+void loop(){
+	while(PowerPush()){
+		DoSumo();
+	}
+	Stop();
+}
 
-boolean FrontLine()
-  {
-  //frontLine get data
-  frontLine_Value = analogRead(frontLine_Pin);
-  if(frontLine_Value < 52){
-    //actual condtions
-    //return true;
-    //testing
-    return true;
-  }
-  else{
-    //actual conditions
-    //return false;
-    //testing
-    return false;
-    
-  }
+void DoSumo(){  
+	//FrontLine true when white, false when black in real conditions
+
+	//main code
+
+	//get current values
+	if(!trigToken){
+		t1 = US();
+		delay(2);
+		t2 = US();
+		delay(2);
+		t3 = US();
+		delay(2);
+	}
+	  
+	if(!FrontLine()){
+		if ((t1 < trigDist && t2 < trigDist && t3 < trigDist) || trigToken){ 
+			trigToken = false; 
+			maxTime = millis() + 1500.0f;
+			while(!FrontLine() && millis() < maxTime){
+			Forward();
+			}
+		}
+	}
+	else{
+		while(!FrontLine() && US() > trigDist){
+			RotateCW();
+		}
+		Stop();
+		if (US() < trigDist){
+			trigToken = true;
+		}
+	}
   
-  }
+	if(FrontLine()){
+		Stop();
+		AvoidBorder();
+	}
+}
 
-void AttachServos(){
-  leftServo.attach(leftServo_Pin);  //attaches the servo on pin to the servo object 
-  rightServo.attach(rightServo_Pin); //attaches the servo on pin to the servo object 
+float US(){
+	//US
+	float cmMsec, inMsec;
+	long microsec = ultrasonic.timing();
+
+	cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);
+	Serial.print("MS: ");
+	Serial.print(microsec);
+	Serial.print(", Distance (cm): ");
+	Serial.println(cmMsec);
+	return cmMsec;
+}
+
+boolean FrontLine(){
+	//frontLine get data
+	rightFrontLine_Value = analogRead(rightFrontLine_Pin);
+	  
+	if(rightFrontLine_Value < 52){
+		//return false;
+		return true;
+	}
+	else{
+		return true;
+	}
 }
 
 void Stop(){
-  //left: 82 == full stop
-  //offset to 90: -8
-  //leftServo.write(82);
-  leftServo.write(90);
-  //right: 81 == full stop
-  //offset to 90: -9
-  //rightServo.write(81);
-  rightServo.write(94);
+	digitalWrite(M1,LOW);
+	digitalWrite(M2, LOW);
+	analogWrite(E1, 0);
+	analogWrite(E2, 0);
 }
 
 void Backward(){
-  rightServo.write(126);
-  leftServo.write(37);
+	digitalWrite(M1,HIGH);
+	digitalWrite(M2, HIGH);
+	analogWrite(E1, 250);
+	analogWrite(E2, 250);
 }
 
 void Forward(){
-  rightServo.write(36);
-  leftServo.write(127);
+	digitalWrite(M1,LOW);
+	digitalWrite(M2, LOW);
+	analogWrite(E1, 250);
+	analogWrite(E2, 250);
 }
 
 void RotateCW(){
-  leftServo.write(105);
-  rightServo.write(111);
+	digitalWrite(M1,LOW);
+	digitalWrite(M2, HIGH);
+	analogWrite(E1, 255);
+	analogWrite(E2, 255);
 }
 
 void RotateCCW(){
-  rightServo.write(36);
-  leftServo.write(36);
+	digitalWrite(M1,HIGH);
+	digitalWrite(M2, LOW);    
+	analogWrite(E1, 255);
+	analogWrite(E2, 255);
 }
 
 void RotateCWSlow(){
-  leftServo.write(92);
-  rightServo.write(92);
+
 }
 
 void RotateCCWSlow(){
-  rightServo.write(72);
-  leftServo.write(72);
+
 }
 
 void RotateCWavg(){
-  leftServo.write(115);
-  rightServo.write(115);
+	digitalWrite(M1,LOW);
+	digitalWrite(M2, HIGH);
+	analogWrite(E1, 255);
+	analogWrite(E2, 255);
 }
 
 void AvoidBorder(){
-  Stop();
-  delay(75);
-  RotateCWavg();
-  delay(275);
-  Stop();
-  delay(75);
-  Forward();
-  delay(350);
-  Stop();
-  delay(75);
+	Stop();
+	//delay(25);
+	RotateCWavg();
+	delay(1250);
+	//Stop();
+	//delay(25);
+	Forward();
+	//delay(450);
+	//Stop();
+	//delay(25);
 }
 
 //not used 
 void EvasiveManeuver(){
-  //when facing enemy for too long
-  Stop();
-  delay(75);
-  RotateCW();
-  delay(700);
-  Stop();
-  delay(75);
-  noBreak = false;
+	//when facing enemy for too long
+	Stop();
+	delay(75);
+	RotateCW();
+	delay(700);
+	Stop();
+	delay(75);
+	noBreak = false;
 }
 
 
--- Robot2/Robot2.ino
+++ Robot2/Robot2.ino
@@ -1,59 +1,251 @@
-//Arduino PWM  Code
-//M1: Direction for Motor1
-//M2: Direction for Motor2
-//E1: PWM control for Motor1
-//E2: PWM control for Motor2
-int E1 = 5;   //left motor PWM   
-int M1 = 4;   //left motor 
-int E2 = 3;   //right motor PWM                   
-int M2 = 2;   //right motor
+//US defintions
+#include <Ultrasonic.h>
+#define TRIGGER_PIN  6
+#define ECHO_PIN     7
+Ultrasonic ultrasonic(TRIGGER_PIN, ECHO_PIN);
 
-void setup() 
-{ 
-    pinMode(M1, OUTPUT);   
-    pinMode(M2, OUTPUT); 
+//MOTOR definitions
+int E1 = 11;   //left motor PWM   
+int M1 = 6;   //left motor 
+int E2 = 9;   //right motor PWM                   
+int M2 = 4;   //right motor
+
+//SERVO definitions
+#include <Servo.h> //include servo libs
+Servo Servo; //create servo object to control a servo 
+int Servo_Pin = 9; //connected to digital out
+
+//FRONT PUSH definitions
+int frontPush_Pin = 10;
+
+//POWER PUSH definitions
+int powerPush_Pin = 8;
+
+//LINE defintions 
+int leftFrontLine_Pin = 0; //connected to analog in 0
+int rightFrontLine_Pin = 1; //connected to analog in 0
+
+int frontLine_Led = 13;
+int leftFrontLine_Value;
+int rightFrontLine_Value;
+int frontLine_Value;
+
+//GLOBAL definitions
+//arena official diameter mini category: 77 cm
+float trigDist = 38.5f;
+boolean trigToken;
+float t1, t2, t3;
+float maxTime;
+boolean noBreak;
+
+void setup(){
+	Serial.begin(9600);
+
+	//startup delay
+	Serial.println("Welcome to Neothug");
+	Serial.println("Starting in 5 seconds...");
+	delay(5000);
+	  
+	pinMode(M1, OUTPUT);   
+	pinMode(M2, OUTPUT); 
+	AttachServo();
+}
+
+void SensorsReport(){
+	US();
+	Serial.print("Left frontLine Sensor: ");
+	Serial.println(analogRead(leftFrontLine_Pin));
+	Serial.print("Right frontLine Sensor: ");
+	Serial.println(analogRead(rightFrontLine_Pin));
+	Serial.print("FrontPush state: ");
+	FrontPush();
+	Serial.print("PowerPush state: ");
+	PowerPush();  
 }
 
 void loop(){
-  testMotors();
+	TestMotors();
 }
 
-void testMotors() 
-{ 
-  int value; 
-  for(value = 0 ; value <= 255; value+=5) 
-  { 
-    digitalWrite(M1,LOW);   
-    digitalWrite(M2, LOW);       
-    analogWrite(E1, value);   //PWM Speed control 
-    analogWrite(E2, value);   //PWM Speed control 
-    delay(30); 
-  }  
+void TestMotors(){ 
+int value; 
+	for(value = 0 ; value <= 255; value+=5){ 
+		digitalWrite(M1,LOW);   
+		digitalWrite(M2, LOW);       
+		analogWrite(E1, value);   //PWM Speed control -> other PIN
+		analogWrite(E2, value);   //PWM Speed control 
+		delay(30); 
+	}  
 }
 
-void turnLeft(){
-    digitalWrite(M1,HIGH);
-    digitalWrite(M2, LOW);    
-    analogWrite(E1, 177);
-    analogWrite(E2, 77);
+void real_loop(){  
+	//FrontLine true when white, false when black in real conditions
+	SensorsReport();
+
+	//main code
+
+	//get current values
+	if(!trigToken){
+		t1 = US();
+		delay(5);
+		t2 = US();
+		delay(5);
+		t3 = US();
+		delay(5);
+	}
+
+	if(!FrontLine()){
+		if ((t1 < trigDist && t2 < trigDist && t3 < trigDist) || trigToken){ 
+			trigToken = false; 
+			maxTime = millis() + 3500.0f;
+			while(!FrontLine() && millis() < maxTime){
+				Forward();
+			}
+		}
+		else{
+			while(!FrontLine() && US() > trigDist){
+				RotateCW();
+			}
+			Stop();
+			if (US() < trigDist){
+				trigToken = true;
+			}
+		}
+	}
+
+	if(FrontLine()){
+	Stop();
+	AvoidBorder();
+	}
 }
 
-void turnRight(){
-    digitalWrite(M1,LOW);
-    digitalWrite(M2, HIGH);
-    analogWrite(E1, 77);
-    analogWrite(E2, 177);
-    
+
+float US(){
+	//US
+	float cmMsec, inMsec;
+	long microsec = ultrasonic.timing();
+
+	cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);
+	Serial.print("MS: ");
+	Serial.print(microsec);
+	Serial.print(", Distance (cm): ");
+	Serial.println(cmMsec);
+	return cmMsec;
 }
 
-void stop(){
+boolean PowerPush(){
+	if (digitalRead(powerPush_Pin) == LOW) {
+		Serial.print("Button is LOW");
+		return false;
+	}
+	else{
+		Serial.print("Button is HIGH");
+		return true;
+	}
+}
+	
+boolean FrontPush(){
+	if (digitalRead(frontPush_Pin) == LOW) {
+		Serial.print("Button is LOW");
+		return false;
+	}
+	else{
+		Serial.print("Button is HIGH");
+		return true;
+	}	
+}  
+  
+boolean FrontLine(){
+	//frontLine get data
+	leftFrontLine_Value = analogRead(leftFrontLine_Pin);
+	rightFrontLine_Value = analogRead(rightFrontLine_Pin);
+	 
+	if(leftFrontLine_Value < 52 || rightFrontLine_Value < 52){
+			return true;
+		}
+		else{
+			return false;
+		}
+}
+
+void AttachServo(){
+	Servo.attach(Servo_Pin);  //attaches the servo on pin to the servo object  
+}
+
+void Stop(){
     digitalWrite(M1,LOW);
     digitalWrite(M2, LOW);
     analogWrite(E1, 0);
     analogWrite(E2, 0);
 }
 
+void Backward(){
+    digitalWrite(M1,HIGH);
+    digitalWrite(M2, HIGH);
+    analogWrite(E1, 230);
+    analogWrite(E2, 230);
+}
 
+void Forward(){
+    digitalWrite(M1,LOW);
+    digitalWrite(M2, LOW);
+    analogWrite(E1, 250);
+    analogWrite(E2, 250);
+}
+
+void RotateCW(){
+    digitalWrite(M1,LOW);
+    digitalWrite(M2, HIGH);
+    analogWrite(E1, 77);
+    analogWrite(E2, 177);
+}
+
+void RotateCCW(){
+    digitalWrite(M1,HIGH);
+    digitalWrite(M2, LOW);    
+    analogWrite(E1, 177);
+    analogWrite(E2, 77);
+}
+
+void RotateCWSlow(){
+
+}
+
+void RotateCCWSlow(){
+
+}
+
+void RotateCWavg(){
+	digitalWrite(M1,LOW);
+    digitalWrite(M2, HIGH);
+    analogWrite(E1, 77);
+    analogWrite(E2, 177);
+}
+
+void AvoidBorder(){
+	Stop();
+	delay(75);
+	RotateCWavg();
+	delay(275);
+	Stop();
+	delay(75);
+	Forward();
+	delay(350);
+	Stop();
+	delay(75);
+}
+
+//not used 
+void EvasiveManeuver(){
+	//when facing enemy for too long
+	Stop();
+	delay(75);
+	RotateCW();
+	delay(700);
+	Stop();
+	delay(75);
+	noBreak = false;
+}	
 
 
 
